31bf99b1f332a5194ac48ae7ce5ab3f05fa55eaa
Joby John <joby432john@gmail.com>
2022-07-27 18:32:08 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringfunctions_org
+{
+      class Class1
+    {
+        public void upper()// function
+        {
+            //using uppercase function
+            string str = ("Whats your name dear dear ?");
+            Console.WriteLine(str.ToUpper());
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace stringfunctions_org
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exploring string functions from next files ");
+            Class1 class1 = new Class1(); // assigning a variable to the class and defining it i think 
+             class1.upper(); //calling the function
+        }
+    }
+}
