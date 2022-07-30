ca11999729be114f54385c4dfd89ad59d1ff9f79
Joby John <joby432john@gmail.com>
2022-07-30 16:38:33 +0530

    more functions

--- Class1.cs
+++ Class1.cs
@@ -13,6 +13,16 @@ namespace stringfunctions_org
             //using uppercase function
             string str = ("Whats your name dear dear ?");
             Console.WriteLine(str.ToUpper());
+            //using lowercase function
+            Console.WriteLine(str.ToLower());
+            //using contain function
+            Console.WriteLine(str.Contains("dear"));
+            //to access using index
+            Console.WriteLine(str[1]);
+            //to find index of something in a string
+            Console.WriteLine( str.IndexOf("name"));//-1 for absent charcaters
+            //to print a substring
+            Console.WriteLine(str.Substring(11,5));//5 is number of characters to grab
         }
     }
 
